14ad5ad99a9e2e9061a66698d729f79f48c9ed57
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-11-23 21:47:57 +1300

    designing info section

--- src/Info.jsx
+++ src/Info.jsx
@@ -2,11 +2,13 @@ import resumePhoto from '../src/assets/resume-photo.png'
 export default function Info() {
   return(
     <div className="info">
-      <img src={resumePhoto} />
+      <img src={resumePhoto} className="info--photo"/>
       <h1 className="info--title">Alyssa.</h1>
       <h3 className="info--title_role">Technology Graduate</h3>
       <a href="https://alyssa-pineda-assignment2-ver2.netlify.app/" className="info--link">My Website</a>
-      <button className="info--email_button">Email</button>
+      <button className="info--button"><span class="material-symbols-outlined">
+      mail
+      </span>Email</button>
     </div>
   )
 }
